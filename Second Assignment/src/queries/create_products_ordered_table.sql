c6fa8d35a46c2082007a10cd70c876f09933b008
Tiago Fonseca <t.fonseca@ua.pt>
2024-04-13 16:42:55 +0100

    Created the Table Queries for the Project, Logged DB Architecture

--- Second Assignment/src/queries/create_products_ordered_table.sql
+++ Second Assignment/src/queries/create_products_ordered_table.sql
@@ -8,7 +8,7 @@ BEGIN
         order_id INT NOT NULL,
         product_id INT NOT NULL,
         quantity INT NOT NULL,
-        PRIMARY KEY (user_id, product_id),
+        PRIMARY KEY (order_id, product_id),
         FOREIGN KEY (order_id) REFERENCES orders(order_id),
         FOREIGN KEY (product_id) REFERENCES products(product_id)
     );
